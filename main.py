7746e634dbae3b23966b193069091fa07650d5f7
Sampada <smsubedii@gmail.com>
2024-01-11 09:18:50 +0545

    Image Stacking

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
 imgCanny= cv2.Canny(imgBlur,10,50)
 
 imageArray = ([img, imgGray,imgBlur,imgCanny])
+imgStacked = functions.stackImages(imageArray,0.3)
 
-cv2.imshow("Stacked",)
+cv2.imshow("Stacked", imgStacked)
 cv2.waitKey(0)
 
 
