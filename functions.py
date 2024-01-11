ff2aff7e1f382c19f99fcfc962301bbac07bcfb3
Sampada <smsubedii@gmail.com>
2024-01-11 10:16:57 +0545

    defining the recContour fucntion

--- functions.py
+++ functions.py
@@ -40,5 +40,16 @@ def stackImages(imgArray,scale,lables=[]):
     return ver
 
 def rectContours(contours):
-    
+    rectCon = []
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if len(approx) == 4:
+                rectCon.append(i)
+    rectCon = sorted(rectCon, key=cv2.contourArea,reverse=True)
+    #print(len(rectCon))
+    return rectCon
 
