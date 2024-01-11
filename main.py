5b880eefa7781d3ffa62bc1f5698ce20b4b50eb3
Sampada <smsubedii@gmail.com>
2024-01-11 09:01:13 +0545

    stacking image

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ heightImg = 600
 ########################################
 img = cv2.imread(path)
 img2 = cv2.resize(img, (widthImg, heightImg))  #image resizing
-#imgContours = img.copy()
-#imgBiggestContours = img.copy()
-#imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
-#imgCanny= cv2.Canny(imgBlur,10,50)
+imgContours = img.copy()
+imgBiggestContours = img.copy()
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
+imgCanny= cv2.Canny(imgBlur,10,50)
 
 cv2.imshow("Stacked",)
 cv2.waitKey(0)
