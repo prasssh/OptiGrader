5ffa149b02de9b461f9839ddde44706dc27b9f95
Sampada <smsubedii@gmail.com>
2024-01-11 09:03:40 +0545

    create image array

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
 imgCanny= cv2.Canny(imgBlur,10,50)
 
+imageArray = ([img, imgGray,imgBlur,imgCanny])
+
 cv2.imshow("Stacked",)
 cv2.waitKey(0)
 
