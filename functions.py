b4736b4b2d955b7803f64ebaba91377a53571342
Sampada <smsubedii@gmail.com>
2024-01-11 08:58:23 +0545

    added sheet for a+scanning

---
+++ functions.py
@@ -0,0 +1,5 @@
+import cv2
+import numpy as np
+
+
+# stacking all the images in a single window
---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import cv2
+import functions
+
+#####################################
+path = "1.jpg"
+widthImg = 600
+heightImg = 600
+
+
+
+########################################
+img = cv2.imread(path)
+img2 = cv2.resize(img, (widthImg, heightImg))  #image resizing
+#imgContours = img.copy()
+#imgBiggestContours = img.copy()
+#imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
+#imgCanny= cv2.Canny(imgBlur,10,50)
+
+cv2.imshow("Stacked",)
+cv2.waitKey(0)
+
+
+
+
+
+
