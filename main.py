350e494dede53016c06490e7be3b35b3989b530c
Sampada <smsubedii@gmail.com>
2024-01-11 10:20:05 +0545

    finding all of the existing contours

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur= cv2.GaussianBlur(imgGray,(5,5),1)
 imgCanny= cv2.Canny(imgBlur,10,50)
 
+#finding contours
 contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) # to find all contours
 cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
 
