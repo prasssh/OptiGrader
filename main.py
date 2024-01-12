c5d9352915965cb8becb12e1885570a255c5dda9
Sampada <smsubedii@gmail.com>
2024-01-12 14:11:02 +0545

    more images

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ while True:
                         (1100, 350), (0, 255, 0), cv2.FILLED)
         cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200,
                                                     int(stackedImage.shape[0] / 2)),
-                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
         cv2.imshow('Result', stackedImage)
         cv2.waitKey(300)
         count += 1
