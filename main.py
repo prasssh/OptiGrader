f45090e98f6335fb63f896d73b079ec942086d0c
Sampada <smsubedii@gmail.com>
2024-01-12 08:26:16 +0545

    img types

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ ans= [1,2,0,2,4]
 count=0
 
 while True:
+    #if webCamFeed:success, img = cap.read()
+    # else:
+    img = cv2.imread(pathImage)
+    
+    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
+    imgFinal = img.copy()
+    imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGGING IF REQUIRED
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
+    imgCanny = cv2.Canny(imgBlur,10,70) # APPLY CANNY 
