422e22c745fde455ca760aae813e766af2f5b2d6
Sampada <smsubedii@gmail.com>
2024-01-12 08:31:40 +0545

    .

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ while True:
     imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGGING IF REQUIRED
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
-    imgCanny = cv2.Canny(imgBlur,10,70) # APPLY CANNY 
+    imgCanny = cv2.Canny(imgBlur,10,70) # APPLY CANNYyy
